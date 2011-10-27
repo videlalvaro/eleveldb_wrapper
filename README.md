005b7c76353bd6707e183defa5d019f68aabc0c9
Alvaro Videla <videlalvaro@gmail.com>
2011-10-27 13:36:11 +0200

    first import

---
+++ README.md
@@ -0,0 +1,3 @@
+# Eleveldb Wrapper #
+
+Wrapper for the RabbitMQ Public Umbrella plugin system to be able to use the `Eleveldb` key/value store with RabbitMQ.
---
+++ package.mk
@@ -0,0 +1,37 @@
+APP_NAME:=eleveldb
+DO_NOT_GENERATE_APP_FILE:=true
+
+UPSTREAM_GIT:=git://github.com/basho/eleveldb.git
+UPSTREAM_REVISION:=eb2342b595b348df42bd18f52d635bd8a0edd7d2
+RETAIN_ORIGINAL_VERSION:=true
+
+ORIGINAL_APP_FILE=$(CLONE_DIR)/src/$(APP_NAME).app.src
+
+LIBRARY:=$(CLONE_DIR)/priv/eleveldb.so
+
+CONSTRUCT_APP_PREREQS:=$(LIBRARY)
+define construct_app_commands
+	mkdir -p $(APP_DIR)/priv
+	cp $(LIBRARY) $(APP_DIR)/priv
+endef
+
+define package_rules
+
+$(LIBRARY):
+	cd $(CLONE_DIR); \
+	make
+
+$(PACKAGE_DIR)+clean::
+	rm -rf $(LIBRARY)
+
+# This is disgusting. Why can't I just depend on _and_ unpack
+# $(EZ_FILE) ? Instead we have .done. targets to confuse matters...
+# The reason for unpacking is that we can't dynamically load libraries
+# that are within .ez files.
+$(PACKAGE_DIR)+pre-run:: $(PACKAGE_DIR)/dist/.done.$(PACKAGE_VERSION)
+	rm -rf $(PACKAGE_DIR)/dist/$(APP_NAME)-$(PACKAGE_VERSION)
+	unzip $(PACKAGE_DIR)/dist/$(APP_NAME)-$(PACKAGE_VERSION).ez -d $(PACKAGE_DIR)/dist
+
+$(PACKAGE_DIR)+pre-test:: $(PACKAGE_DIR)+pre-run
+
+endef
