1cd5d1c26096b405e1a90b0770e3dc03d5530f7b
Carl Hörberg <carl.hoerberg@gmail.com>
2014-07-25 08:23:30 +0200

    use upstream version 2.0.0beta1 and set version explicitly

--- package.mk
+++ package.mk
@@ -2,7 +2,8 @@ APP_NAME:=eleveldb
 DO_NOT_GENERATE_APP_FILE:=true
 
 UPSTREAM_GIT:=git://github.com/basho/eleveldb.git
-UPSTREAM_REVISION:=master
+UPSTREAM_REVISION:=2.0.0beta1
+ORIGINAL_VERSION:=2.0.0beta1
 RETAIN_ORIGINAL_VERSION:=true
 
 ORIGINAL_APP_FILE=$(CLONE_DIR)/src/$(APP_NAME).app.src
